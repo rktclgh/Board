c4e332b9f95c62d7e320cc8ea198da4f995053fc
rktclgh <songchih@icloud.com>
2025-05-30 21:14:01 +0900

    0530

---
+++ Board/src/test/java/practiceProducts/vol1/Board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package practiceProducts.vol1.Board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
